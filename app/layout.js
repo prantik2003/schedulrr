5b32aa34ac3ebf220e29378faac161476ed69d58
Prantik Guin <145896649+prantik2003@users.noreply.github.com>
2025-06-03 12:47:34 +0530

    updates

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
           </main>
           <footer className="bg-blue-100 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with 💗 by Prantik Guin</p>
+              <p>© 2025 <strong>Schedulrr</strong> | All rights reserved.</p>
             </div>
           </footer>
           <CreateEventDrawer />
